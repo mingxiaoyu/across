ff4c4a1267360f317f1001b7c170a5a158236256
fengying0347 <fengying0347@163.com>
2021-01-06 21:07:50 +0800

    caddy

--- v2ray/entrypoint.sh
+++ v2ray/entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
 echo "modify v2ray config ..."
-sed -i "s@V2_UUID@${V2_UUID}@g" /etc/v2ray/config.json
-sed -i "s@V2_WS_PATH_VLESS@${V2_WS_PATH_VLESS}@g" /etc/v2ray/config.json
+sed -i "s@V2_UUID@${V2_UUID}@g" /v2/config.json
+sed -i "s@V2_WS_PATH_VLESS@${V2_WS_PATH_VLESS}@g" /v2/config.json
 echo "modify v2ray config completed"
 
 if  [[ $CADDY = "true" ]];
 then
 echo "run caddy ..."
-/usr/bin/caddy start
-curl localhost:2019/load \
-  -X POST \
-  -H "Content-Type: application/json" \
-  -d @caddy.json
+chmod +x ./caddy
+./caddy start
+#curl localhost:2019/load \
+#  -X POST \
+#  -H "Content-Type: application/json" \
+#  -d @caddy.json
 echo "run caddy completed"
 fi
 
 echo "run v2ray"
-/usr/bin/xray -config /etc/v2ray/config.json
+./xray -config config.json
